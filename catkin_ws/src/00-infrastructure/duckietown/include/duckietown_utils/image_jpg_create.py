52001eaf6c6bccf50fe6fbdf6ffd4281e3adcff1
ldzenan <ldzenan@bitbucket.org>
2017-10-01 01:23:14 +0200

    fixed an error in image_jpg_create.py

--- catkin_ws/src/00-infrastructure/duckietown/include/duckietown_utils/image_jpg_create.py
+++ catkin_ws/src/00-infrastructure/duckietown/include/duckietown_utils/image_jpg_create.py
@@ -2,14 +2,15 @@
 
 
 def d8_compressed_image_from_cv_image(image_cv):
-    """ 
+    """
         Create CompressedIamge from a CV image.
-    
+
         TODO: assumptions on format?
     """
     import cv2
     import numpy as np
-    from sensor_msgs import CompressedImage  # @UnresolvedImport
+    import rospy
+    from sensor_msgs.msg import CompressedImage  # @UnresolvedImport
     compress =  cv2.imencode('.jpg', image_cv)[1]
     jpg_data = np.array(compress).tostring()
     msg = CompressedImage()
